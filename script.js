29609381737ae82d327159f12eec49a8acc53703
Dextavision <dextavision@gmail.com>
2019-11-19 18:38:10 +0100

    added new features: shows actual background css at loading and added a random color generator button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector("button");
 
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
@@ -9,6 +10,16 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function randomColors() {
+    color1.value = "#"+((1<<24)*Math.random()|0).toString(16);
+    color2.value = "#"+((1<<24)*Math.random()|0).toString(16);
+    setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", randomColors);
+
+document.addEventListener("DOMContentLoaded", setGradient);
